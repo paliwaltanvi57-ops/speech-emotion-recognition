e8d0de0e0256d5f9a9dc0effc3ae6c332c606804
Tanvi Paliwal <paliwaltanvi57@gmail.com>
2026-07-17 23:41:43 +0530

    Deploy CNN speech emotion recognition model in Flask application

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 import os
+import json
 from utils.chunk_audio import split_audio
 from utils.predict_emotion import predict_emotion
-import json
+
 
 
 app = Flask(__name__)
@@ -43,33 +44,37 @@ def upload():
 
     for chunk in chunk_files:
         chunk_path = os.path.join(chunk_folder, chunk)
-
         emotion, confidence = predict_emotion(chunk_path)
-
+        print(f"Chunk: {chunk}")
+        print(f"Emotion: {emotion}")
+        print(f"Confidence: {confidence:.2f}")
+        print("-----------------------")
         results.append({
             "chunk": chunk,
             "emotion": emotion,
-            "confidence": confidence})
+            "confidence": round(confidence, 2)
+        })
 
-        print(f"Chunk: {chunk}")
-        print(f"Path: {chunk_path}")
-        print(f"Emotion: {emotion}")
-        print(f"Confidence: {confidence}")
-        print("-------------------------")
+
     print(results)
+
+    os.makedirs("results", exist_ok=True)
+
     result_file = os.path.join("results", "emotion_analysis.json")
     with open(result_file, "w") as json_file:
         json.dump(results, json_file, indent=4)
     
-    return f"""<h2>Upload Successful ✅</h2>
-    <p>File: {file.filename}</p>
-    <p>Total Chunks Created: {num_chunks}</p>"""
+    overall_emotion = max(
+    results,
+    key=lambda x: x["confidence"]
+)["emotion"]
+    return f"""
+    <h2>Upload Successful ✅</h2>
+    <p><b>File:</b> {file.filename}</p>
+    <p><b>Total Chunks:</b> {num_chunks}</p>
+    <p><b>Overall Emotion:</b> {overall_emotion}</p>
+    """
 
-     # Predict emotions
-
-     # Save results
-
-     # Return response
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils/predict_emotion.py
+++ utils/predict_emotion.py
@@ -1,16 +1,50 @@
+import numpy as np
+import librosa
+import tensorflow as tf
+import joblib
+
+model = tf.keras.models.load_model("models/emotion_model.keras")
+label_encoder = joblib.load("models/label_encoder.pkl")
+
+print("Model Loaded Successfully!")
+print("Label Encoder Loaded Successfully!")
 
 def predict_emotion(audio_path):
-    """
-    Predict the emotion of a single audio file.
 
-    Parameters:
-        audio_path (str): Path of the audio file.
+    # Load audio
+    signal, sample_rate = librosa.load(audio_path, sr=None)
 
-    Returns:
-        tuple: (emotion, confidence)
-    """
+    # Extract MFCC
+    mfcc = librosa.feature.mfcc(
+        y=signal,
+        sr=sample_rate,
+        n_mfcc=40
+    )
 
-    emotion = "Happy"
-    confidence = 0.91
+    # Make MFCC width = 300 (same as training)
+    if mfcc.shape[1] > 300:
+        mfcc = mfcc[:, :300]
+    else:
+        pad_width = 300 - mfcc.shape[1]
+        mfcc = np.pad(
+            mfcc,
+            ((0, 0), (0, pad_width)),
+            mode="constant"
+        )
+
+    # Add channel dimension
+    mfcc = mfcc[np.newaxis, ..., np.newaxis]
+
+    # Predict
+    prediction = model.predict(mfcc, verbose=0)
+
+    # Highest probability
+    predicted_index = np.argmax(prediction)
+
+    confidence = float(np.max(prediction))
+
+    # Convert number back to emotion name
+    emotion = str(label_encoder.inverse_transform([predicted_index])[0])
 
     return emotion, confidence
+
